7743807671935d5f95d2b9be5dbce24082ee007a
Frain <chenhuangyun@linglongtech.com>
2018-05-17 11:29:54 +0800

    更新脚本

--- work/nimitz/startall.bat
+++ work/nimitz/startall.bat
@@ -1,7 +1,7 @@
 @echo off
-rem call nimitz-semantic-baike.bat
+call nimitz-semantic-baike.bat
 call nimitz-semantic-calc.bat
-rem call nimitz-semantic-customizatin.bat
+call nimitz-semantic-customizatin.bat
 call nimitz-semantic-dispatch.bat
 call nimitz-semantic-faq.bat
 call nimitz-semantic-lbs.bat
@@ -10,26 +10,26 @@ call nimitz-semantic-news.bat
 call nimitz-semantic-normal.bat
 call nimitz-semantic-open.bat
 call nimitz-semantic-rcu.bat
-rem call nimitz-semantic-smarthome.bat
-rem call nimitz-semantic-stock.bat
-rem call nimitz-semantic-translation.bat
+call nimitz-semantic-smarthome.bat
+call nimitz-semantic-stock.bat
+call nimitz-semantic-translation.bat
 call nimitz-semantic-userchat.bat
 
-rem call nimitz-flow-baike.bat
+call nimitz-flow-baike.bat
 call nimitz-flow-calc.bat
-rem call nimitz-flow-customizatin.bat
+call nimitz-flow-customizatin.bat
 call nimitz-flow-dispatch.bat
 call nimitz-flow-faq.bat
 call nimitz-flow-lbs.bat
 call nimitz-flow-limit.bat
 call nimitz-flow-news.bat
-rem call nimitz-flow-normal.bat
+call nimitz-flow-normal.bat
 call nimitz-flow-songsearch.bat
 call nimitz-flow-open.bat
 call nimitz-flow-rcu.bat
-rem call nimitz-flow-smarthome.bat
-rem call nimitz-flow-stock.bat
-rem call nimitz-flow-translation.bat
+call nimitz-flow-smarthome.bat
+call nimitz-flow-stock.bat
+call nimitz-flow-translation.bat
 call nimitz-flow-userchat.bat
 
 call nimitz-dispatch.bat
