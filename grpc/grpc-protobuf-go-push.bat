2ece91ac64a6cabb33da938649f060a498ae5ecf
Frain <chenhuangyun@linglongtech.com>
2018-11-05 10:56:34 +0800

    修改协议脚本

---
+++ grpc/grpc-protobuf-go-push.bat
@@ -0,0 +1,14 @@
+set PROTOC_BIN_PATH=C:\opt\Protobuf\protoc-3.5.1-win32\bin
+set PROTOC_PATH=C:\repository\git\project\nimitz_source_server\nimitz\nimitz_protoc_file\go\nimitz_push
+set GO_OUT=C:\repository\git\project\nimitz_source_server\nimitz\nimitz_protoc_file\go
+set GO_PATH=C:\opt\Gopath
+
+cd /d %PROTOC_PATH%
+
+protoc --go_out=plugins=grpc:.  nimitz_push.proto
+
+REM protoc --plugin=protoc-gen-go=%GO_PATH%\bin\protoc-gen-go.exe --go_out=%GO_OUT% --proto_path=%PROTOC_PATH% nimitz_semantic.proto
+
+echo "Success."
+
+pause
